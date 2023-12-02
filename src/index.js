f25def895231c6e151088b005a01ce20c54a4fc2
Milson Fortunato Neto <milsonfn@yahoo.com>
2023-12-02 11:41:04 -0300

    EstilosGlobais

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './containers/Login';
+import GlobalStyles from './styles/globalStyles';
 
 ReactDOM.render(
-  <Login />,
+  <>
+    <Login />
+    <GlobalStyles />  
+  </>,
+
   document.getElementById('root')
 );
