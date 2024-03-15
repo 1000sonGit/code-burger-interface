33e8ad1927c368d5190c915fa2784388890fee8f
Milson Fortunato Neto <milsonfn@yahoo.com>
2024-03-15 13:48:40 -0300

    EstilizandoCarrinho

--- src/components/CartItens/index.js
+++ src/components/CartItens/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import { useCart } from "../../hooks/CartContext"
-import { Container, Header, Body } from './styles'
+import { Container, Header, Body, EmptyCart } from './styles'
 import formatCurrency from "../../utils/formatCurrency"
 
 export function CartItens() {
@@ -13,19 +13,22 @@ export function CartItens() {
                 <p></p>
                 <p>Order Details</p>
                 <p>Price</p>
-                <p>Quantity</p>
+                <p style={{ paddingRight: 30 }}>Quantity</p>
                 <p>Total</p>
             </Header>
 
-            {cartProducts && cartProducts.map( product => (
-            <Body key={product.id}>
-                <img src={product.url}/>
-                <p>{product.name}</p>
-                <p>{formatCurrency(product.price)}</p>
-                <p>{product.quantity}</p>
-                <p>{formatCurrency(product.quantity * product.price)}</p>
-            </Body>
-            ))}
+            {cartProducts && cartProducts.length > 0 ? (
+                cartProducts.map( product => (
+                    <Body key={product.id}>
+                        <img src={product.url}/>
+                        <p>{product.name}</p>
+                        <p>{formatCurrency(product.price)}</p>
+                        <p>{product.quantity}</p>
+                        <p>{formatCurrency(product.quantity * product.price)}</p>
+                    </Body>
+            ))) : (
+                <EmptyCart>Empty Cart</EmptyCart>
+            )}
             
         </Container>
 
